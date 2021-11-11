237492063d196c3bf0d6a13110c8e7b4c5f95e44
zanevc5555 <zvancampfort@gmail.com>
2021-11-11 15:48:08 +1300

    #

--- fill_dict.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-#Fill values dictionary
-Fill_dict = {"Year": np.nan, "Day": np.nan, "Hour": np.nan, "Bartels": 9999, "ID-IMF": 99, "ID-SW": 99, "nop-IMF": 999, "nop-plasma": 999, "FMA": 999.9, "Mag-AFV": 999.9, "Lat-AFV": 999.9, "Long-AFV": 999.9, "Bx": 999.9, "By GSE": 999.9, "Bz GSE": 999.9, "By GSM": 999.9, "Bz GSM": 999.9,
-"sigma absB": 999.9, "sigma B": 999.9, "sigma Bx": 999.9, "sigma By": 999.9, "sigma Bz": 999.9, "ProTemp": 9999999., "ProDens": 999.9, "FlowSpeed": 9999, "FlowLong": 999.9, "FlowLat": 999.9, "Na/Np": 9.999, "FlowPres": 99.99, "sigma T": 9999999., "sigma N": 999.9, "sigma V": 9999.,
-"sigma pV": 999.9, "sigma tV": 999.9, "sigma Na/Np": 9.999, "Efield": 999.99, "Pbeta": 999.99, "Alfven": 999.9, "Kp": 99, "R": 999, "DST": 99999, "AE": 9999, "Pflux-1": 999999.99, "Pflux-2": 99999.99, "Pflux-4": 99999.99, "Pflux-10": 99999.99, "Pflux-30": 99999.99, "Pflux-60": 99999.99,
-"ap": 999, "f10.7": 999.9, "PC(N)": 999.9, "AL": 99999, "AU": 99999, "Mgsonic": 99.9}
