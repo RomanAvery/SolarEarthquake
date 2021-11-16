d8c7ee2a266ec520343a9da4d8c8a046484387cc
zanevc5555 <zvancampfort@gmail.com>
2021-11-16 18:51:01 +1300

    Added lyman-alpha and QI variables, date-time column, and descriptions for some variables

--- load_omni2_data.py
+++ load_omni2_data.py
@@ -10,6 +10,7 @@ Created on Wed Nov  3 14:40:42 2021
 import pandas as pd
 import numpy as np
 from load_omni2_info import omni2_info
+from datetime import datetime
 
 columns = {
     "Yr": 1,  # 1, Year
@@ -40,46 +41,62 @@ columns = {
     "longPFA": 26,  # 26, Plasma Flow Long. Angle
     "latPFA": 27,  # 27, Plasma Flow Lat. Angle
     "NaNp": 28,  # 28, Na/Np
-    "FP": 29,  # 29, Flow Pressure
-    "sT": 30,  # 30, sigma T
-    "sN": 31,  # 31, sigma N
-    "sV": 32,  # 32, sigma V
-    "sPV": 33,  # 33, sigma phi V
-    "sTV": 34,  # 34, sigma theta V
-    "sNaNp": 35,  # 35, sigma-Na/Np
+    "sT": 29,  # 29, sigma T
+    "sN": 30,  # 30, sigma N
+    "sV": 31,  # 31, sigma V
+    "sPV": 32,  # 32, sigma phi V
+    "sTV": 33,  # 33, sigma theta V
+    "sNaNp": 34,  # 34, sigma-Na/Np
+    "FP": 35,  # 35, Flow Pressure
     "EF": 36,  # 36, Electric field
     "PB": 37,  # 37, Plasma beta
     "AMN": 38,  # 38, Alfven mach number
-    "Kp": 39,   # 39, Planetary geomagnetic activity index (Kp)
-    "R": 40,   # 40, Sunspot number (R)
-    "DST": 41,  # 41, DST Index
-    "AE": 42,  # 42, AE-index
-    "PF1": 43,  # 43, Proton flux >1 MeV
-    "PF2": 44,  # 44, Proton flux >2 MeV
-    "PF4": 45,  # 45, Proton flux >4 MeV
-    "PF10": 46,  # 46, Proton flux >10 MeV
-    "PF30": 47,  # 47, Proton flux >30 MeV
-    "PF60": 48,  # 48, Proton flux >60 MeV
-    "F": 49,  # 49, Flag(***)
-    "ap": 50,  # 50, ap-index
-    "f10.7": 51,  # 51, f10.7_index
-    "PCN": 52,  # 52, PC(N) index
-    "AL": 53,  # 53, AL-index, from Kyoto
-    "AU": 54,  # 54, AU-index, from Kyoto
-    "MMN": 55,  # 55, Magnetosonic mach number
+    "MMN": 39,  # 39, Magnetosonic mach number
+    "QI": 40,  # 40, Proton Quasy-Invariant
+    "Kp": 41,   # 41, Planetary geomagnetic activity index (Kp)
+    "R": 42,   # 42, Sunspot number (R)
+    "DST": 43,  # 43, DST Index
+    "ap": 44,  # 44, ap-index
+    "f10.7": 45,  # 45, f10.7_index
+    "AE": 46,  # 46, AE-index
+    "AL": 47,  # 47, AL-index, from Kyoto
+    "AU": 48,  # 48, AU-index, from Kyoto
+    "PCN": 49,  # 49, PC(N) index
+    "LyAlp": 50,  # 50, Solar Lyman-alpha intensity
+    "PF1": 51,  # 51, Proton flux >1 MeV
+    "PF2": 52,  # 52, Proton flux >2 MeV
+    "PF4": 53,  # 53, Proton flux >4 MeV
+    "PF10": 54,  # 54, Proton flux >10 MeV
+    "PF30": 55,  # 55, Proton flux >30 MeV
+    "PF60": 56,  # 56, Proton flux >60 MeV
+    "F": 57,  # 57, Flag(***)  
 }
 
-omni2 = pd.read_table('data/omni2_all_years.dat.txt', sep='\s+', names=columns.keys())
+#Work-around for a bug where names parameter(line 77) does not accept columns.keys() as a valid input
+colnames = []
+for pair in columns:
+    colnames.append(pair)
 
+#Date-time format as input for date_parser parameter
+custom_date_parser = lambda x: datetime.strptime(x, "%Y %j %H")
+
+omni2 = pd.read_table('data/omni2_all_data2.txt', sep='\s+', names=colnames, dtype={"Yr": "object", "DD": "object", "Hr": "object"}, parse_dates={"Date": ["Yr", "DD", "Hr"]}, keep_date_col=True, dayfirst=True, date_parser=custom_date_parser)
+
+
+columns["Date"]=0
 # Replace bad flags with np.nan
 for column in omni2:
     # Get value to fill
-    columnKey = columns[column] - 1
+    columnKey = columns[column]
     fillValue = omni2_info['FILL_VALUE'][columnKey]
     
-    print('Column name: {}, fill value: {}'.format(column, fillValue));
+   # print('Column name: {}, fill value: {}'.format(column, fillValue));
     
     # Now replace fillValue with nan
     omni2.loc[omni2[column] == fillValue,column] = np.nan
-    
-#omni2.head().plot(x="Yr", y=['Kp', 'R'])
+ 
+##Test Functions##
+
+#print(omni2[2000:2003])
+#omni2.head().plot(x="Date", y=['Kp', 'R'])
+#omni2.to_csv("data/omni2_full.txt", sep='\t')
