372bad34fcd0253ee6dfa8ad9337d2994bed666b
Roman Avery <roman@cinemanager.co.nz>
2021-11-25 15:38:43 +1300

    Fixed merge code.

--- roman-merge.py
+++ roman-merge.py
@@ -32,82 +32,65 @@ For each earthquake event date and each desired variable:
 - merge into new dataframe with event timestamp, lat, long, depth, and magnitude
 """
 
-def create_dataframe(initial_vars, wanted_vars, wanted_props):
-    # Make empty dataframe
-    data = pd.DataFrame()
+def get_columns(initial_vars, wanted_vars, wanted_props):
+    columns = []
     
     # Add initial 
     for var in initial_vars:
-        data[var] = np.nan
+        columns.append(var)
         
     # For each wanted variable, add a column with each needed property
     for var in wanted_vars:
         for prop in wanted_props:
-            data[var + '-' + prop] = np.nan
+            columns.append(var + '-' + prop)
     
-    # Return DataFrame
-    return data
+    # Return columns
+    return columns
 
-def calculate_range(out_data, omni2, event, event_vals, vals, props, start, end):
+def calculate_range(omni2, event, event_vals, vals, props, start, end):
     data_slice = omni2.loc[(omni2['Date'] >= start) & (omni2['Date'] <= end)]
     
     new_data = {}
     
-    #for val in event_vals:
-    #    new_data[val] = event[val]
-    new_data['date'] = event['date']
+    for val in event_vals:
+        new_data[val] = event[val]
     
     # Calculate for each needed variable
     for val in vals:
-        MEAN = 0.0
-        MEAN_COUNT = 0
-        MAX = -999999.0
-        MIN = 999999.0
-        DELTA = 0.0
-        SD = 0.0
-        
-        # Iterate each row of the slice
-        for index, row in data_slice.iterrows():
-            # Calc mean
-            MEAN += row[val]
-            MEAN_COUNT += 1
-            
-            # Calc max
-            if row[val] > MAX:
-                MAX = row[val]
-            
-            # Calc min
-            if row[val] < MIN:
-                MIN = row[val]
-          
-        
-        if MEAN_COUNT > 0:
-            MEAN = MEAN / MEAN_COUNT
-        else:
-            MEAN = 0
-            
-        new_data[val + '-MEAN'] = MEAN
-        
-        # Calc delta
-        DELTA = MAX - MIN
-        new_data[val + '-DELTA'] = DELTA
-        
-    # Add data to the DataFrame
-    out_data.append(new_data, ignore_index=True)
+        new_data[val + '-MEAN'] = data_slice[val].mean()
+        new_data[val + '-MAX'] = data_slice[val].max()
+        new_data[val + '-DELTA'] = data_slice[val].max() - data_slice[val].min()
+        new_data[val + '-SD'] = data_slice[val].std()
+    
+    return new_data
 
 
 # Lists of variables and properties we want
-event_vals = ['date', 'lat', 'long', 'depth', 'mw']
+event_vals = ['date', 'lat', 'lon', 'depth', 'mw']
 calc_vals = ['magAFV', 'sB', 'PT', 'PD', 'PFS', 'FP', 'EF', 'Kp', 'DST']
 calc_props = ['MEAN', 'MAX', 'DELTA', 'SD']
 
-out_data = create_dataframe(event_vals, calc_vals, calc_props)
+print('Starting events')
+count_t = 0
+
+rows = []
 
 for index, row in earthquake.iterrows():
     date = row['date']
     end_date = dp.parse(date)
     start_date = end_date - datetime.timedelta(hours = hours_before)
     
-    calculate_range(out_data, omni2, row, event_vals, calc_vals, calc_props, start_date, end_date)
+    count_t += 1
+    print('Event #', count_t)
     
+    calc_row = calculate_range(omni2, row, event_vals, calc_vals, calc_props, start_date, end_date)
+    rows.append(calc_row)
+    
+
+#print('Creating column names')
+#out_columns = get_columns(event_vals, calc_vals, calc_props)
+    
+# Adding to DataFrame
+print('Creating DataFrame')
+out_data = pd.DataFrame(rows)
     
